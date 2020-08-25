bf095a51e7e968951879ec433791a3a126128a31
Albin Jose <albinavirappatt@gmail.com>
2020-08-25 12:39:56 +0530

    code optimized

--- unique-id.go
+++ unique-id.go
@@ -2,7 +2,6 @@ package uniqueid
 
 import (
 	"errors"
-	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -22,75 +21,46 @@ func Generateid(params ...interface{}) (string, error) {
 		size = params[1].(int)
 	}
 
+	y := strconv.Itoa(time.Now().Year())[2:]
+	m := strconv.Itoa(int(time.Now().Month()))
+	d := strconv.Itoa(time.Now().Day())
+	h := strconv.Itoa(time.Now().Hour())
+	t := strconv.Itoa(time.Now().Minute())
+	s := strconv.Itoa(time.Now().Second())[:1]
+	n := strconv.Itoa(time.Now().Nanosecond())[2:4]
+	p := strconv.Itoa(os.Getpid())[2:]
+
+	if len(m) == 1 {
+		m = "0" + m
+	}
+	if len(d) == 1 {
+		d = "0" + d
+	}
+	if len(h) == 1 {
+		h = "0" + h
+	}
+	if len(t) == 1 {
+		t = "0" + t
+	}
+	uid := n + p + s + t + h + d + m + y
+	if size > 16 {
+		narray := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+		b := make([]string, size-16)
+		for i := range b {
+			b[i] = narray[rand.Intn(len(narray))]
+		}
+		uid = uid + strings.Join(b, "")
+	}
+	if size < 16 {
+		uid = uid[:size]
+	}
+
 	switch params[0] {
 	case "i":
-		// generate number id
-		y := strconv.Itoa(time.Now().Year())[2:]
-		m := strconv.Itoa(int(time.Now().Month()))
-		d := strconv.Itoa(time.Now().Day())
-		h := strconv.Itoa(time.Now().Hour())
-		t := strconv.Itoa(time.Now().Minute())
-		s := strconv.Itoa(time.Now().Second())[:1]
-		n := strconv.Itoa(time.Now().Nanosecond())[2:4]
-		p := strconv.Itoa(os.Getpid())[2:]
-		if len(m) == 1 {
-			m = "0" + m
-		}
-		if len(d) == 1 {
-			d = "0" + d
-		}
-		if len(h) == 1 {
-			h = "0" + h
-		}
-		if len(t) == 1 {
-			t = "0" + t
-		}
-		uid := n + p + s + t + h + d + m + y
-		if size > 16 {
-			narray := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
-			b := make([]string, size-16)
-			for i := range b {
-				b[i] = narray[rand.Intn(len(narray))]
-			}
-			uid = uid + strings.Join(b, "")
-		}
-		return uid[:size], nil
+		return uid, nil
 	case "a":
-		// generate number id
 		alphabets := []string{"a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"}
-		y := strconv.Itoa(time.Now().Year())[2:]
-		m := strconv.Itoa(int(time.Now().Month()))
-		d := strconv.Itoa(time.Now().Day())
-		h := strconv.Itoa(time.Now().Hour())
-		t := strconv.Itoa(time.Now().Minute())
-		s := strconv.Itoa(time.Now().Second())[:1]
-		n := strconv.Itoa(time.Now().Nanosecond())[2:4]
-		p := strconv.Itoa(os.Getpid())[2:]
 
-		if len(m) == 1 {
-			m = "0" + m
-		}
-		if len(d) == 1 {
-			d = "0" + d
-		}
-		if len(h) == 1 {
-			h = "0" + h
-		}
-		if len(t) == 1 {
-			t = "0" + t
-		}
-		uid := n + p + s + t + h + d + m + y
-		if size > 16 {
-			narray := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
-			b := make([]string, size-16)
-			for i := range b {
-				b[i] = narray[rand.Intn(len(narray))]
-			}
-			uid = uid + strings.Join(b, "")
-		}
-		if size < 16 {
-			uid = uid[:size]
-		}
 		uidarray := strings.Split(uid, "")
 		stringuidarray := make([]string, size)
 		for i := 0; i < len(uidarray); i++ {
@@ -99,8 +69,7 @@ func Generateid(params ...interface{}) (string, error) {
 			stringuidarray[i] = alphabets[j]
 		}
 		stringuid := strings.Join(stringuidarray, "")
-		fmt.Println("stringuid: ", stringuid)
-		return stringuid[:size], nil
+		return stringuid, nil
 	default:
 		return "", errors.New("invalid type specified")
 	}
