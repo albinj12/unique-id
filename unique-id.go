73129260fe144e03a3a8adefcb821f18ca02fddb
Albin Jose <albinavirappatt@gmail.com>
2020-08-25 14:26:17 +0530

    change options

--- unique-id.go
+++ unique-id.go
@@ -59,9 +59,9 @@ func Generateid(params ...interface{}) (string, error) {
 	}
 
 	switch params[0] {
-	case "i":
+	case "n":
 		return uid, nil
-	case "a":
+	case "l":
 		alphabets := []string{"a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"}
 
 		uidarray := strings.Split(uid, "")
@@ -73,7 +73,7 @@ func Generateid(params ...interface{}) (string, error) {
 		}
 		stringuid := strings.Join(stringuidarray, "")
 		return stringuid, nil
-	case "n":
+	case "a":
 		alphabets := []string{"a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"}
 		totalletters := math.Floor(0.7 * float64(size))
 		randarray := make([]int, int(totalletters))
