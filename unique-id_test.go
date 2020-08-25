3fc626e97ee14b9358c1beb0ae89c03e58500996
Albin Jose <albinavirappatt@gmail.com>
2020-08-25 19:24:49 +0530

    add check for testing type of number id

--- unique-id_test.go
+++ unique-id_test.go
@@ -2,6 +2,8 @@ package uniqueid
 
 import (
 	"math/rand"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -14,6 +16,22 @@ func TestLength(t *testing.T) {
 	}
 	if (len(id)) != length {
 		t.Errorf("got %d, want %d", len(id), length)
+	}
+}
+
+// test for checking type
+func TestType(t *testing.T) {
+	length := rand.Intn(100)
+	id, err := Generateid("n", length)
+	if err != nil {
+		t.Error("error while generating id.", err)
+	}
+	idarray := strings.Split(id, "")
+	for _, v := range idarray {
+		_, err := strconv.Atoi(v)
+		if err != nil {
+			t.Error("id is not number only")
+		}
 	}
 }
 
